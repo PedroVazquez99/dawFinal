72d0b91090352d93d136f3d3406db83fcaadf68c
JorgeBernabeuCaballero <jbc85@gcloud.ua.es>
2025-03-24 22:52:38 +0100

    modificacion del home

--- taller-be/CreacionBD.sql
+++ taller-be/CreacionBD.sql
@@ -48,3 +48,17 @@ VALUES (
     CONVERT(VARCHAR(250), HASHBYTES('SHA2_256', '1234'), 2),
     'admin'
 );
+
+-- Servicios
+
+INSERT INTO Servicio (Nombre, Descripcion, Precio, Duracion)
+VALUES
+    ('Corte', 'Corte de cabello clásico.', 15.00, '00:30:00'),
+    ('Corte + Barba', 'Corte de cabello y arreglo de barba.', 25.00, '01:00:00'),
+    ('Arreglo de Barba', 'Recorte y arreglo de barba.', 10.00, '00:30:00');
+    ('Corte infantil', 'Corte de cabello especializado para niños.', 15.00, '00:30'),
+    ('Tratamiento capilar', 'Tratamiento profundo para la hidratación y reparación del cabello.', 40.00, '01:00'),
+    ('Alisado permanente', 'Alisado profesional para mantener el cabello liso por meses.', 120.00, '02:30'),
+    ('Peinado de novia', 'Peinado exclusivo para bodas y eventos especiales.', 80.00, '01:30'),
+    ('Extensiones de cabello', 'Colocación de extensiones para agregar longitud y volumen.', 150.00, '03:00'),
+    ('Coloración completa', 'Aplicación de tinte en todo el cabello para un nuevo look.', 70.00, '02:00');
